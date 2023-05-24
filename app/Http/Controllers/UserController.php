cf251520854c2c26314abb399f92159c646af169
thuyquanlucchien <linhthuydanhbo1975@gmail.com>
2023-05-24 23:41:52 +0900

    day du an 2 len github

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function show(){
+        return view('auth.add');
+    }
+
+    public function store(Request $request){
+
+    }
+}
